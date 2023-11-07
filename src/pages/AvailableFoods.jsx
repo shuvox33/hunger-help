f898a0daf27020053f08e712f9ac8f8279a3e727
shuvox33 <shuvox33@gmail.com>
2023-11-07 23:05:13 +0600

    fix some bug for featured foods

--- src/pages/AvailableFoods.jsx
+++ src/pages/AvailableFoods.jsx
@@ -16,10 +16,22 @@ const AvailableFoods = () => {
 
 
     return (
-        <div className=" max-w-7xl mx-auto grid grid-cols-4 mt-10 gap-5">
-            {
-                foods.map(food =><ShowAvailableFoods key={food._id} food = {food}></ShowAvailableFoods>)
-            }
+        <div className=" max-w-7xl mx-auto mt-10">
+            <div className=" flex justify-evenly">
+                <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" name="searchText" placeholder="Search…" className="input input-bordered" />
+                        <button className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 mt-10 gap-5">
+                {
+                    foods.map(food => <ShowAvailableFoods key={food._id} food={food}></ShowAvailableFoods>)
+                }
+            </div>
         </div>
     );
 };
