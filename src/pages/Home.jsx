5220b11edf7699cf668d1771a4d9a9c89b13648f
shuvox33 <shuvox33@gmail.com>
2023-11-08 11:51:24 +0600

    added two extra feature section structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import Banner from "../components/Banner";
 import FeatureFoods from "../components/FeatureFoods";
+import Lottie from 'lottie-react';
+import animationData from '../../public/Animation.json';
+import animation1Data from '../../public/Animation 1.json';
 
 
 const Home = () => {
@@ -7,6 +10,33 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <FeatureFoods></FeatureFoods>
+            <div className=" max-w-7xl mx-auto  mt-20">
+                <div className="flex ">
+                    <div className="w-1/2 ">
+                        <Lottie className="h-[400px] " animationData={animationData} />
+                    </div>
+                    <div className="w-1/2 flex flex-col  justify-center">
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary ">Get Started</button>
+                    </div>
+                </div>
+            </div>
+            <div className=" max-w-7xl mx-auto  mt-20">
+                <div className="flex flex-row-reverse">
+                    <div className="w-1/2 ">
+                        <Lottie className="h-[400px] " animationData={animation1Data} />
+                    </div>
+                    <div className="w-1/2 flex flex-col  justify-center">
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-primary ">Get Started</button>
+                    </div>
+                </div>
+            </div>
+
+            
+
         </div>
     );
 };
